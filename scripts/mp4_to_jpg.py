eda2c9f01868fad06cfb248e52e47d7f152910da
egm42 <31607146+egm42@users.noreply.github.com>
2020-03-01 11:55:11 -0500

    mp4 to jpg update
    Script converst videos into JPGs using ffmpeg. It also creates an SRT file using ffmpeg. Scripts makes use of argparse to take in 3 args: -i is input file, -o is output directory and -m is mode. Currently only implemented mode 'a' which will create the SRT file; default is just the images.

--- scripts/mp4_to_jpg.py
+++ scripts/mp4_to_jpg.py
@@ -1,19 +1,29 @@
 import os
 import argparse
 import subprocess
+from pathlib import Path
+import ffmpeg
 
-def mp4_to_jpg(filepath, dir, mode):
+def mp4_to_jpg(filename, dir, mode):
     print('Starting MP4 processing...')
-    if(mode):
-        print('Basic Mode')
+    file = Path(filename).stem
+    image = ffmpeg.input(filename)
+    image2 = ffmpeg.output(image, os.path.join(dir, file + '_%dimage.jpg'))
+    ffmpeg.run(image2)
+
+    if (mode == 'a'):
+        input = ffmpeg.input(filename)
+        output_file = os.path.join(dir, file + '.srt')
+        output = ffmpeg.output(input, output_file, map='0:2', y='-y')
+        ffmpeg.run(output)
     else:
-        print('Advanced Mode')
+        pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert MP4s into JPGs')
     parser.add_argument('-i', '--input_file', type=str, required=True, help='MP4 filepath')
     parser.add_argument('-o', '--output_dir', type=str, required=True, help='Output directory')
-    parser.add_argument('-b', '--basic', type=bool, nargs='?', const=True, default=False, help='Toogle to True to only extract JPGs')
+    parser.add_argument('-m', '--mode', type=str, default=False, help='Set mode to \'a\' to create SRT file')
     args = parser.parse_args()
 
-    mp4_to_jpg(args.input_file, args.output_dir, args.basic)
+    mp4_to_jpg(args.input_file, args.output_dir, args.mode)
